b9295d4e8d832b689a79f9f125fc88980437d305
Luzian Scherrer <luzian@neoos.ch>
2023-12-04 11:06:17 +0100

    formatting

--- day04b.py
+++ day04b.py
@@ -8,7 +8,6 @@ card = df.iloc[:, margin + 1 :].to_numpy()
 wins = np.array([np.sum(np.isin(win[i], card[i])) for i in range(win.shape[0])])
 
 piles = np.ones(len(wins), dtype=int)
-
 for i, cs in enumerate(piles):
     for w in range(wins[i]):
         piles[i + 1 + w] += cs
