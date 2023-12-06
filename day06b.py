3c05cb5bc892eefb07ea2aa6ddbbd030e9baa7da
Luzian Scherrer <luzian@neoos.ch>
2023-12-06 09:00:19 +0100

    cleanup

--- day06b.py
+++ day06b.py
@@ -3,7 +3,5 @@ import re
 with open("day06input.txt", "r") as file:
     data = file.readlines()
 
-t = int(re.sub("[^\d]", "", data[0]))
-d = int(re.sub("[^\d]", "", data[1]))
-
+t, d = [int(re.sub("[^\d]", "", d)) for d in data]
 print(sum([1 if i * (t - i) > d else 0 for i in range(1, t)]))
