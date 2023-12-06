c356f91b8d2b2c2569e3a83a4721e2d2d29377c9
Luzian Scherrer <luzian@neoos.ch>
2023-12-06 09:03:34 +0100

    cleanup

--- day06b.py
+++ day06b.py
@@ -4,4 +4,4 @@ with open("day06input.txt", "r") as file:
     data = file.readlines()
 
 t, d = [int(re.sub("[^\d]", "", d)) for d in data]
-print(sum([1 if i * (t - i) > d else 0 for i in range(1, t)]))
+print(sum([i * (t - i) > d for i in range(1, t)]))
