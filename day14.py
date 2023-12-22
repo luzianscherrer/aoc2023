d3073d7ca287b7612eae922357e32ada6d675c36
Luzian Scherrer <luzian@neoos.ch>
2023-12-22 12:39:10 +0100

    day 14

---
+++ day14.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+m = np.genfromtxt("day14input.txt", dtype=bytes, comments=None, delimiter=1).astype(str)
+
+
+def show(m):
+    print(np.sum(m == "O", axis=1) @ np.arange(m.shape[0], 0, -1))
+
+
+def tilt(m):
+    for offset in range(1, m.shape[0]):
+        for row in range(m.shape[0] - offset):
+            selection = (m[row, :] == ".") & (m[row + 1, :] == "O")
+            m[row, selection] = "O"
+            m[row + 1, selection] = "."
+
+
+cycles, lookup, found, i = 1000000000 * 4, {}, False, 0
+while True:
+    tilt(np.rot90(m, (4 - i) % 4))
+    if i == 0:
+        show(m)
+    if found == False:
+        check = hash(m.data.tobytes())
+        if check in lookup:
+            found = True
+            i = cycles - (cycles - i) % (i - lookup[check])
+        else:
+            lookup[check] = i
+    i += 1
+    if i == cycles:
+        break
+show(m)
