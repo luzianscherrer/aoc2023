fc51de6e6ce543898ff80ee315f733b101e9cd93
Luzian Scherrer <luzian@neoos.ch>
2023-12-26 14:32:07 +0100

    day 17

--- day17b.py
+++ day17b.py
@@ -2,6 +2,7 @@ import heapq, numpy as np
 
 opposites = {"U": "D", "D": "U", "L": "R", "R": "L"}
 directions = [((-1, 0), "U"), ((0, 1), "R"), ((1, 0), "D"), ((0, -1), "L")]
+prohibits = ["U", "R", "D", "L"]
 
 m = np.pad(np.genfromtxt("day17input.txt", dtype=np.uint8, delimiter=1), 1)
 
@@ -23,16 +24,18 @@ def solve(idx, target):
     while len(queue):
         curdist, curidx, curdir = heapq.heappop(queue)
 
-        if len(curdir) == 0:
-            excl = []
+        if len(curdir) and (len(curdir[-4:]) != 4 or len(set(curdir[-4:])) != 1):
+            excl = prohibits[:]
+            excl.remove(curdir[-1])
         else:
-            excl = [opposites[curdir[-1]]]
-        if len(curdir) == 3 and len(set(curdir)) == 1:
+            excl = []
+
+        if len(curdir) == 10 and len(set(curdir)) == 1:
             excl.append(curdir[0])
 
         for neighbor in neighbors(curidx, exclude=excl):
             distance = curdist + neighbor[1]
-            nextdir = curdir[-2:]
+            nextdir = curdir[-9:]
             nextdir.append(neighbor[2])
 
             if neighbor[0] == target:
