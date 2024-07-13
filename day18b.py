b047a5c1facc2c68656643cd3408738fbdd8ecc0
Luzian Scherrer <luzian@scherrer.ch>
2024-07-13 13:52:07 +0200

    cleanup

--- day18b.py
+++ day18b.py
@@ -3,25 +3,7 @@ import numpy as np
 
 df = pd.read_csv("day18input.txt", header=None, sep=" ")
 
-maxrow, minrow, maxcol, mincol, col, row = 0, 0, 0, 0, 0, 0
-for i, entry in df.iterrows():
-    dir = entry.iloc[2][-2]
-    dist = int(entry.iloc[2][2:-2], 16)
-    if dir == "0":
-        col += dist
-        maxcol = max(maxcol, col)
-    elif dir == "2":
-        col -= dist
-        mincol = min(mincol, col)
-    elif dir == "3":
-        row -= dist
-        minrow = min(minrow, row)
-    elif dir == "1":
-        row += dist
-        maxrow = max(maxrow, row)
-
-m = np.zeros((abs(minrow) + maxrow + 1, abs(mincol) + maxcol + 1), dtype=np.uint8)
-pos = (abs(minrow), abs(mincol))
+pos = (0, 0)
 
 turns = [pos]
 perimeter = 0
@@ -29,18 +11,14 @@ for i, entry in df.iterrows():
     length = int(entry.iloc[2][2:-2], 16)
     direction = int(entry.iloc[2][-2])
     if direction == 0:
-        m[pos[0], pos[1] : length + 1] = 1
         pos = (pos[0], pos[1] + length)
         perimeter += length
     elif direction == 1:
-        m[pos[0] : length + 1, pos[1]] = 1
         pos = (pos[0] + length, pos[1])
         perimeter += length
     elif direction == 2:
-        m[pos[0], pos[1] - length : length + 1] = 1
         pos = (pos[0], pos[1] - length)
     elif direction == 3:
-        m[pos[0] - length : length + 1, pos[1]] = 1
         pos = (pos[0] - length, pos[1])
     turns.append(pos)
 
