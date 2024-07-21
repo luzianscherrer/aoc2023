de3934047d3066e793027a0580876349b589699a
Luzian Scherrer <luzian@scherrer.ch>
2024-07-21 19:54:14 +0200

    day21a

---
+++ day21a.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+m = np.genfromtxt("day21input.txt", dtype=bytes, comments=None, delimiter=1).astype(str)
+m[m == "S"] = "O"
+
+
+def step(m):
+    for coord in np.transpose((m == "O").nonzero()):
+        if coord[0] - 1 >= 0 and m[coord[0] - 1, coord[1]] != "#":
+            m[coord[0] - 1, coord[1]] = "O"
+        if coord[0] + 1 < m.shape[0] and m[coord[0] + 1, coord[1]] != "#":
+            m[coord[0] + 1, coord[1]] = "O"
+        if coord[1] - 1 >= 0 and m[coord[0], coord[1] - 1] != "#":
+            m[coord[0], coord[1] - 1] = "O"
+        if coord[1] + 1 < m.shape[1] and m[coord[0], coord[1] + 1] != "#":
+            m[coord[0], coord[1] + 1] = "O"
+        m[*coord] = "."
+
+
+for i in range(64):
+    step(m)
+print(np.count_nonzero(m == "O"))
