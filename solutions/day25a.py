55ec1c40016898ecc57ac20ec1e9cfab80dccff3
Luzian Scherrer <luzian@neoos.ch>
2024-07-26 09:57:20 +0200

    day25a

--- solutions/day25a.py
+++ solutions/day25a.py
@@ -1,19 +1,8 @@
 import networkx as nx
-from itertools import combinations
 
-filename = "day25example.txt"
-with open(filename, "r") as file:
+with open("day25input.txt", "r") as file:
     lines = [line.replace(":", "").rstrip() for line in file]
 
 G = nx.parse_adjlist(lines)
-for edges in combinations(G.edges, 3):
-    for edge in edges:
-        G.remove_edge(*edge)
-    if nx.number_connected_components(G) == 2:
-        print(
-            (len(nx.descendants(G, edge[0])) + 1)
-            * (len(nx.descendants(G, edge[1])) + 1)
-        )
-        break
-    for edge in edges:
-        G.add_edge(*edge)
+_, partition = nx.stoer_wagner(G)
+print(len(partition[0]) * len(partition[1]))
